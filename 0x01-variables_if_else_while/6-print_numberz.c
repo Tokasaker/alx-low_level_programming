9f9eca86d77974d86650093adae7400effdca781
Tokasaker <tokasakr88@gmail.com>
2023-07-15 14:44:44 -0700

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: "print all numbers from 0 to 10"
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	while (n < 10)
+	{
+		putchar("%d", n);
+		n++;
+	}
+	putchar("\n");
+	return (0);
+}
