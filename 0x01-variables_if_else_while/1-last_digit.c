d3f7db665f2d150a59fd43e3345062615e84b5d8
Tokasaker <tokasakr88@gmail.com>
2023-07-15 11:39:17 -0700

    a

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %i is %i and is 0\n", n, d);
 	}
-	else if (d < 6 &&  != 0)
+	else if (d < 6 && d != 0)
 	{
 		printf("Last digit of %i is %i and is and is less than 6 and not 0\n", n, d);
 	}
