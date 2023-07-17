1aaa180ce79a2a959422311ddacab999178c5b13
Tokasaker <tokasakr88@gmail.com>
2023-07-17 13:06:22 -0700

    a

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 1 (error)
+*/
+
+int main(void)
+{
+	putchar("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	putchar("\n");
+
+	return (1);
+}
