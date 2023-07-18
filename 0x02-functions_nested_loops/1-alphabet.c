ab69d9a7bd94ddf405d820763ec01c51913b0565
Tokasaker <tokasakr88@gmail.com>
2023-07-18 15:35:47 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphapet in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n')
+}
