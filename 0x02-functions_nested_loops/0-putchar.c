0798217e8e9fa4d84fa35a2aeee529dc1d56e202
Tokasaker <tokasakr88@gmail.com>
2023-07-18 15:25:46 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  * Description: prints _putchar using putchar prototype
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	char str[] = "_putchar";
 
-	int ch
+	int ch;
 
 	for (ch = 0; ch < 8; ch++)
 		_putchar(str[ch]);
