f555de18bb7d4d69c104c6936626a42ea94a757c
Tokasaker <tokasakr88@gmail.com>
2023-07-18 15:09:21 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * @c: the character to print
  *
  * Return: on success 1.
- * 	   on error , -1 is reruned, and errno is set appropiately.
+ *	   on error , -1 is reruned, and errno is set appropiately.
  */
 int _putchar(char c)
 {
