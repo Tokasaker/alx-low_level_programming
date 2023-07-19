e2b5ed6b0c8221c9be54b7b467da196eebdc02dc
Tokasaker <tokasakr88@gmail.com>
2023-07-19 14:38:16 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- *
  * prints_alphapet - in lowercase from a to z
  *
+ * Description: "print from a to z"
+ *
+ * Return: Always 0 (Success)
 */
 void print_alphabet(void)
 {
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function to check if caharcter is lowercase
+ *
+ * Description: print hours and minutes of a day
+ *
+ * Return: Always 0 (Success)
+*/
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
