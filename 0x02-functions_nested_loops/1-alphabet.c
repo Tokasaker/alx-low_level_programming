38e56ad913af6210a01f8276670cfd702cf6601b
Tokasaker <tokasakr88@gmail.com>
2023-07-19 15:05:07 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * prints_alphapet - in lowercase from a to z
+ * print_alphabet - function to check if caharcter is lowercase
  *
- * Description: "print from a to z"
+ * Description: print alphabet fro a to z
  *
  * Return: Always 0 (Success)
 */
+
 void print_alphabet(void)
 {
 	int ch;
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - function to check if caharcter is lowercase
+ *
+ * Description: print hours and minutes of a day
+ *
+ * Return: Always 0 (Success)
+*/
+
+void times_table(void)
+{
+	int table, time, prod;
+
+	for (table = 0; table <= 9; table++)
+	{
+		_putchar(48);
+		for (time = 0; time <= 0; time++)
+		{
+			_putchar(',');
+			_putchar(',');
+
+			prod = table * time;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
