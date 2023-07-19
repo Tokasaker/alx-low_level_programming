9a6cb71a52744c880c7d13ac96dd8f9c2966e7b8
Tokasaker <tokasakr88@gmail.com>
2023-07-19 12:39:04 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - function to check if caharcter is lowercase
+ *
+ * @n: check value of n
+ *
+ * Return: Always 0 (Success)
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
