15803c32f6495b54490ed819cb651a1034d8e36c
Tokasaker <tokasakr88@gmail.com>
2023-07-20 14:11:13 -0700

    a

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers - check the code
+ *
+ * Description: print numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	i = 0;
+	for (i = 0; i <= 9; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
+
