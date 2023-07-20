74a1a164eb975dcefbdb6c1722bc6cd10eb9afe7
Tokasaker <tokasakr88@gmail.com>
2023-07-20 16:48:20 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,10 +15,14 @@ int row, column;
 if (size <= 0)
 {
 	_putchar('\n');
+} else
+{
 	for (row = 1; row <= size; row++)
 	{
 		for (column = 1; column <= size; column++)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
