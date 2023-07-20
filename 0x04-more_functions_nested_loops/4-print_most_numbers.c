38e5df82bb294c6c218d7847ddfad5271f23d5a5
Tokasaker <tokasakr88@gmail.com>
2023-07-20 14:57:53 -0700

    a

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i >= 0; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
