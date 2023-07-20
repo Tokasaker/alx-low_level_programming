c279dcab72b7985f28f3632f4c68c3b3361aabec
Tokasaker <tokasakr88@gmail.com>
2023-07-20 14:04:51 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ *
+ * @a: f input
+ * @b: s input
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
