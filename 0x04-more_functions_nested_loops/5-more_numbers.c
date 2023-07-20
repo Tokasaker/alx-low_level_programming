d014c4a8d8e28ca6515341707064fde8c7d23f7b
Tokasaker <tokasakr88@gmail.com>
2023-07-20 16:58:18 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,17 +9,16 @@
 
 void more_numbers(void)
 {
-	int table, num, count;
+	int table, num;
 
 	for (table = 1; table <= 10; table++)
 	{
-		for (count = 0; count <= 14; count++)
+		for (num = 0; num <= 14; num++)
 		{
-			num = count;
-			if (count > 9)
+			if (num > 9)
 			{
-				_putchar(1 + 48);
-			_putchar(count % 10 + 48);
+				_putchar(1 + '0');
+			_putchar(num % 10 + '0');
 		}
 		_putchar('\n');
 	}
