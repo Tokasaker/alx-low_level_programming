b27c736b27e27d292abef272e287cbce394ec2ac
Tokasaker <tokasakr88@gmail.com>
2023-07-20 14:32:45 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,19 @@
 /**
  * print_numbers - check the code
- *
- * Description: print numbers from 0 to 9
+ * 		only using _putchar twice
  *
  * Return: Always 0.
  */
 
 void print_numbers(void)
 {
-	int i = 0;
+int i = 0;
 
-	do {
-		_putchar(i + 48);
-		i++;
-	} while (i >= 0 && i <= 9);
-	_putchar('\n');
+do {
+	_putchar(i + 48);
+	i++;
+} while (i >= 0 && i <= 9);
+_putchar('\n');
 }
 
 
