295fa1431a20cf012cfb09d5b39de844b1d68bf9
Tokasaker <tokasakr88@gmail.com>
2023-07-21 03:57:46 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,20 +13,15 @@ void print_diagonal(int n)
 int i, space;
 
 if (n <= 0)
-{
-
 	_putchar('\n');
-}
 else
 {
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		for (space = 0; space <= i; space++)
-		{
+		for (space = 0; space < i; space++)
 			_putchar(' ');
 		_putchar(92);
-		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
 }
