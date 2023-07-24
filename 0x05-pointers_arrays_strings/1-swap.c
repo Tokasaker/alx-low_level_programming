0b4c0a656b6a7b4a64cf249473bbe39ca03df3b7
Tokasaker <tokasakr88@gmail.com>
2023-07-24 15:14:33 -0700

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - takes a paramter to an int paramter
+ *              and updates the value of int to print 98
+ *
+ * @a: int paramyer
+ * @b: second int paramyer
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int t;
+
+	 t = *a;
+
+	*a = *b;
+
+	*b = temp;
+}
