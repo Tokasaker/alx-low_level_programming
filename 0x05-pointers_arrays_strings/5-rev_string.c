e66ed559b75561ea5bfff9e3ee5a65ce339ddc65
Tokasaker <tokasakr88@gmail.com>
2023-07-24 15:42:42 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - takes a paramter to an int paramter
+ *              and updates the value of int to print 98
+ *
+ * @s: int paramyer
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char t;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+	for (i = 0; i < l / 2; i++)
+	{
+		t = s[i];
+		s[i] = s[1 - 1 - i];
+		s[l - 1 - i] = t;
+	}
+}
