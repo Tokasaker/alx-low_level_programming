cb449fe9eb5c785f22dc22d0ec721f9725e98515
Tokasaker <tokasakr88@gmail.com>
2023-07-24 15:50:10 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - takes a paramter to an int paramter
+ *              and updates the value of int to print 98
+ *
+ * @str: int paramyer
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
