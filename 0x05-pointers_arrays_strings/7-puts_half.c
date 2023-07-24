d5d7fdcb0bad8ec94ed38ef0613dd7cd2ff6257d
Tokasaker <tokasakr88@gmail.com>
2023-07-24 15:57:20 -0700

    a

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - takes a paramter to an int paramter
+ *              and updates the value of int to print 98
+ *
+ * @str: int paramyer
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n';)
+}
