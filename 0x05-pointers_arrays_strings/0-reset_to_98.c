aba701a69a26e86a2a4401b5678b2a057207d0e9
Tokasaker <tokasakr88@gmail.com>
2023-07-24 15:06:28 -0700

    a

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a paramter to an int paramter
+ *		and updates the value of int to print 98
+ *
+ * @n: int paramyer
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
