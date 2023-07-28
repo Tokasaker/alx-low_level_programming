3d21ac0281221b9d2b41845cd5ec01105329d250
Tokasaker <tokasakr88@gmail.com>
2023-07-28 09:32:38 -0700

    a

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,13 +12,13 @@
 
 char *string_toupper(char *str)
 {
-	int c;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] - 32
+			str[i] = str[i] - 32;
 		}
 	}
 	return (str);
