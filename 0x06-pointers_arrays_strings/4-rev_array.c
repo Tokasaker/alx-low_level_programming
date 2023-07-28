c05f49d85633d9a366c3c7346e430685c799ce7d
Tokasaker <tokasakr88@gmail.com>
2023-07-28 09:02:04 -0700

    a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - contact two strings
+ *              using putchar
+ *
+ * @a: first string
+ * @n: s string
+ *
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0, j = (n - 1); i < j; i++; j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		t = a[j];
+	}
+}
