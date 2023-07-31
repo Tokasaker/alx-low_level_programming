7ac6c7e57125cdf1d43e583d32c0489d73f27be9
Tokasaker <tokasakr88@gmail.com>
2023-07-31 14:40:40 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * *_strchr - prints buffer in hexa
@@ -14,12 +13,13 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] >= '\0'; i++)
 	{
 		if (src[i] == c)
 		{
 			return (s + i);
 		}
 	}
+
 	return ('\0');
 }
