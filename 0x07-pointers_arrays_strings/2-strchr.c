2bff17b86e16955e9a34af96171c5711d72f1ab4
Tokasaker <tokasakr88@gmail.com>
2023-07-31 14:37:52 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - prints buffer in hexa
+ *
+ * @s: the size of the memory to print
+ * @c: the numbers
+ *
+ * Return: Nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		if (src[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
+}
