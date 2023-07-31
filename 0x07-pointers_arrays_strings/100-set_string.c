6eb7fba86f3467d626122eb48aadcebd148c7be5
Tokasaker <tokasakr88@gmail.com>
2023-07-31 15:37:29 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - prints buffer in hexa
+ *
+ * @s: the size of the memory to print
+ * @to: the numbers
+ *
+ * Return: Nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
