35fb2f0db097a5de63008b9455cbd57bf764c3eb
Tokasaker <tokasakr88@gmail.com>
2023-08-03 13:36:01 -0700

    a

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print reverse string
+ *
+ * @s: the array
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	if (*s != '\0')
+	{
+		i += _strlen_recursion(s + 1) + 1;
+	}
+	return (i);
+}
