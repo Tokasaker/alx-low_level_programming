e76e625ce049e24f1814edc8406636aa99d9a2c8
Tokasaker <tokasakr88@gmail.com>
2023-08-03 13:26:15 -0700

    a

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse string
+ *
+ * @s: the array
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s - 1);
+		_putchar(*s);
+	}
+}
+
