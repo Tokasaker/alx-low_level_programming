8e393436c6970e5defa5819e4e13df7c2c69da6c
Tokasaker <tokasakr88@gmail.com>
2023-08-09 09:52:49 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,17 +9,17 @@
 
 int main(int argc, char const **argv)
 {
-	int i, x, z = 1, 2;
+	int z;
 
-	if (argc != 3)
+	if (argc == 3)
+	{
+		z = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", z)
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
-	}
-	else if (argc == 3)
-	{
-		z = atoi(argv[i]) * atoi(argv[x]);
-		printf("%d\n", z);
 	}
 	return (0);
 }
