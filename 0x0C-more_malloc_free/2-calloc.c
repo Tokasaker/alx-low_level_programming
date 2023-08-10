21819de1a1a082053671094090fe2d2379742037
Tokasaker <tokasakr88@gmail.com>
2023-08-10 15:25:16 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 
 /**
+ * *calloc - using malloc to allocate memory
+ *
+ * @x: the zize of pointer
+ * @j: the size of bytes
+ * @y: the integer
+ *
+ * Return: check if the pointer of the array is zero or not.
+ */
+
+char *calloc(char *j, char x, unsigned int y)
+{
+	char *o = j;
+
+	while (y--)
+		*j++ = x;
+
+	return (o);
+}
+
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
  * *_calloc - using malloc to allocate memory
  *
  * @nmemb: the zize of pointer
@@ -12,11 +36,15 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *h = malloc(nmemb * size);
+	void *h = malloc(sizeof(int) * nmemb);
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 	if (h == 0)
 		return (0);
+
+	*calloc(h, 0, sizeof(int) * nmemb);
+
+	return (h);
 }
 
