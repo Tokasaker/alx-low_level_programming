8ee15667b52b5c85be12e59e20faa9dd96c07a29
Tokasaker <tokasakr88@gmail.com>
2023-08-11 14:03:27 -0700

    a

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,22 +15,28 @@ int **alloc_grid(int width, int height)
 	int **h;
 	int j, k;
 
-	if (width <= 0 || height <= 0)
-		return (0);
-
 	h = malloc(sizeof(*h) * height);
 
+	if (width <= 0 || height <= 0)
+                return (0);
+
 	if (h == 0)
-		return (0);
-	for (j = 0; j < height; j++)
 	{
-		h[j] = malloc(sizeof(**h) * width);
-		if (h[j] == 0)
+		return (0);
+	}
+	else
+	{
+		for (j = 0; j < height; j++)
 		{
+	
+			h[j] = malloc(sizeof(**h) * width);
+			if (h[j] == 0)
+			{
 			while (j--)
 				free(h[j]);
 			free(h);
 			return (0);
+			}
 		}
 		for (k = 0; j < width; j++)
 			h[j][k] = 0;
