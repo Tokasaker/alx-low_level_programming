da579ec3c1f7c42ebc4fd7032af2ca066fbe062b
Tokasaker <tokasakr88@gmail.com>
2023-08-14 15:38:16 -0700

    d

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * init_dog - check the code
+ * @d: the pointer
+ * @name: name of dog
+ * @owner: name of owner is dog
+ * @age: age of dog
+ *
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		*d.name = name;
+		*d.age = age;
+		*d.owner = owner;
+	}
+}
+
