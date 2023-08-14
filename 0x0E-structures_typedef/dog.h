d6064618903a54156c5ed431caa1ada907b05e60
Tokasaker <tokasakr88@gmail.com>
2023-08-14 14:23:04 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - check the code
+ * @name : name of dog
+ * @owner: name of owner of dog
+ * @age: dog is age
+ *
+ * Return: Always 0.
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
+
