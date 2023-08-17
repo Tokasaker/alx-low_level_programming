27f5500d82e095c70cdb1306d044e5ec4c9ab3b3
Tokasaker <tokasakr88@gmail.com>
2023-08-17 16:25:36 -0700

    a

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,25 +4,23 @@
  * sum_them_all - check the code
  *
  * @n: the no. of args
+ * @...: the last no. of args
  *
  * Return: Always 0.
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int counter;
 	int summation = 0;
 	va_list lis;
 
 	if (n == 0)
-	{
 		return (0);
-	}
-	else
-	{
-		va_start(lis, n);
-		for (int counter = 0; counter < n - 1; counter++)
-			summation += va_arg(lis, int);
-		va_end(lis);
-		return (summation);
-	}
+	va_start(lis, n);
+	for (counter = 0; counter < n; counter++)
+			summation += va_arg(lis, unsigned int);
+
+	va_end(lis);
+	return (summation);
 }
