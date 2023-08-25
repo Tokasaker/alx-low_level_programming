f27fe42ef6b479201042b9a6e14d49fda9ddddc0
Tokasaker <tokasakr88@gmail.com>
2023-08-25 05:29:29 -0700

    a

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * yoube - check the code
+ *
+ * Return: Always 0.
+ */
+void yoube(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * hello - check the code
+ *
+ * Return: Always 0.
+ */
+void hello(void)
+{
+        printf("Hello, Holberton");
+}
